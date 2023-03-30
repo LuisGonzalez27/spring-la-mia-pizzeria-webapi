3f29a36062b4f60b8213905da1d394341bbc8234
LuisGonzalez27 <luis27coding@gmail.com>
2023-03-30 14:56:00 +0200

    Aggiunte pagine di errore

---
+++ pizzeria/src/main/java/org/learning/pizzeria/controller/IndexController.java
@@ -0,0 +1,17 @@
+package org.learning.pizzeria.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+    @GetMapping
+    public String index(){
+        //throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED, "My error");
+        return "redirect:/pizze";
+    }
+}
