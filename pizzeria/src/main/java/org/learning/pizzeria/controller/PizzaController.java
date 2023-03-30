f95e29b2266b01665003ce80a27bbfd5e7e840d7
LuisGonzalez27 <luis27coding@gmail.com>
2023-03-30 16:57:04 +0200

    Aggiunto show per pizza

--- pizzeria/src/main/java/org/learning/pizzeria/controller/PizzaController.java
+++ pizzeria/src/main/java/org/learning/pizzeria/controller/PizzaController.java
@@ -3,12 +3,16 @@ package org.learning.pizzeria.controller;
 import org.learning.pizzeria.model.Pizza;
 import org.learning.pizzeria.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/pizze")
@@ -22,5 +26,16 @@ public class PizzaController {
         model.addAttribute("list", listaPizze);
 
         return "/pizze/index";
+    }
+
+    @GetMapping("/{pizzeId}")
+    public String show(@PathVariable("pizzeId") Integer id, Model model){
+        Optional<Pizza> result = pizzaRepository.findById(id);
+        if(result.isPresent()){
+            model.addAttribute("pizza", result.get());
+            return "/pizze/show";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 }
