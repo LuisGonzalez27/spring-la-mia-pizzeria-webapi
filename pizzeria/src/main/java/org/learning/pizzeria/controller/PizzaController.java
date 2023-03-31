7eb56bbeab0b6f76a8062e956d947e37d6732bf7
LuisGonzalez27 <luis27coding@gmail.com>
2023-03-31 15:03:11 +0200

    Form per nuova pizza

--- pizzeria/src/main/java/org/learning/pizzeria/controller/PizzaController.java
+++ pizzeria/src/main/java/org/learning/pizzeria/controller/PizzaController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -38,6 +35,19 @@ public class PizzaController {
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
+    }
+
+    @GetMapping("/create")
+    public String create(Model model){
+        model.addAttribute("pizza", new Pizza());
+        return "/pizze/create";
+    }
+
+    @PostMapping("/create")
+    public String doCreate(@ModelAttribute("Pizza") Pizza formPizza){
+        //VALIDATION
+        pizzaRepository.save(formPizza);
+        return "redirect:/pizze";
     }
 
     @GetMapping("/search")
